c840aa299de8b2fb96f0ee7f8b50a7f366712d19
sureshreddyrayapati <resuresh0@gmail.com>
2024-02-15 15:01:22 +0530

    Add project files.

---
+++ Palindrome/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Palindrome;
+Console.WriteLine("Check a number is palindrome or not");
+CheckPalindrome obj=new CheckPalindrome();
+Console.WriteLine(obj.Paly(125));
